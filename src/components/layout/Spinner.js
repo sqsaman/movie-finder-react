32c21b211773163a3e1811b0baa20d8a8e34ce2c
sqsaman <50292365+sqsaman@users.noreply.github.com>
2020-07-04 10:09:18 +0530

    spinner-image

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,11 +1,14 @@
 import React from "react";
-import spinner from "./spinner.gif";
+//import spinner from "./spinner.gif";
 
 function Spinner() {
   return (
     <div>
       <img
-        src={spinner}
+        //src={spinner}
+        src={
+          "https://i0.wp.com/codemyui.com/wp-content/uploads/2015/09/spinner-loader-animation.gif?fit=880%2C440&ssl=1"
+        }
         style={{ width: "200px", margin: "auto", display: "block" }}
         alt="Loading..."
       />
